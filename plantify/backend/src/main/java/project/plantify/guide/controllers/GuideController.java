9e88a26b6570f0b036998f53bd8520bbd2acef86
CountToFour <247806@edu.p.lodz.pl>
2025-04-11 00:44:02 +0200

    exclude not important info from plants

--- plantify/backend/src/main/java/project/plantify/guide/controllers/GuideController.java
+++ plantify/backend/src/main/java/project/plantify/guide/controllers/GuideController.java
@@ -2,9 +2,14 @@ package project.plantify.guide.controllers;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import project.plantify.guide.playloads.request.FindSpeciesRequest;
+import project.plantify.guide.playloads.response.PlantsResponse;
+import project.plantify.guide.playloads.response.PlantsResponseToFrontend;
 import project.plantify.guide.services.GuideService;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -16,24 +21,24 @@ public class GuideController {
     private GuideService guideService;
 
     @GetMapping("/getAll")
-    public String getAllPlants() {
-        String response = this.guideService.getAllPlant();
+    public ResponseEntity<List<PlantsResponseToFrontend>> getAllPlants() {
+        List<PlantsResponseToFrontend> response = this.guideService.getAllPlant();
         System.out.println(response);
-        return response;
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/getPlantsBySpecies")
-    public String getAllPlantsBySpecies(@RequestBody FindSpeciesRequest req) {
+    public ResponseEntity<List<PlantsResponseToFrontend>> getAllPlantsBySpecies(@RequestBody FindSpeciesRequest req) {
         String species = req.getSpecies();
-        String response = this.guideService.getAllPlantsBySpecies(species);
+        List<PlantsResponseToFrontend> response = this.guideService.getAllPlantsBySpecies(species);
         System.out.println(response);
-        return response;
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/getSinglePlant")
-    public String getSinglePlant(@RequestParam("id") String id) {
-        String response = this.guideService.getSinglePlant(id);
+    public ResponseEntity<PlantsResponseToFrontend> getSinglePlant(@RequestParam("id") String id) {
+        PlantsResponseToFrontend response = this.guideService.getSinglePlant(id);
         System.out.println(response);
-        return response;
+        return ResponseEntity.ok(response);
     }
 }
--- plantify/backend/src/main/java/project/plantify/guide/services/GuideService.java
+++ plantify/backend/src/main/java/project/plantify/guide/services/GuideService.java
@@ -5,6 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import project.plantify.guide.playloads.response.PlantsResponse;
+import project.plantify.guide.playloads.response.PlantsResponseToFrontend;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Service
 public class GuideService {
@@ -15,37 +21,60 @@ public class GuideService {
     @Value("${plant.api.token}")
     private String apiToken;
 
-    public String getAllPlant() {
-        return webClient.get()
+    public List<PlantsResponseToFrontend> getAllPlant() {
+        PlantsResponse plants =  webClient.get()
                 .uri(uriBuilder -> uriBuilder
                         .path("/species-list")
                         .queryParam("k", apiToken)
                         .build())
                 .retrieve()
-                .bodyToMono(String.class)
+                .bodyToMono(PlantsResponse.class)
                 .block();
+
+        return preparePlantsForFronted(Objects.requireNonNull(plants).getData());
     }
 
-    public String getAllPlantsBySpecies(String species) {
-        return webClient.get()
+    public List<PlantsResponseToFrontend> getAllPlantsBySpecies(String species) {
+        PlantsResponse plants = webClient.get()
                 .uri(uriBuilder -> uriBuilder
                         .path("/species-list")
                         .queryParam("key", apiToken)
                         .queryParam("q", species)
                         .build())
                 .retrieve()
-                .bodyToMono(String.class)
+                .bodyToMono(PlantsResponse.class)
                 .block();
+
+        return preparePlantsForFronted(Objects.requireNonNull(plants).getData());
     }
 
-    public String getSinglePlant(String id) {
-        return webClient.get()
+    public PlantsResponseToFrontend getSinglePlant(String id) {
+        PlantsResponse.Plant plant = webClient.get()
                 .uri(uriBuilder -> uriBuilder
                         .path("/species/details/").path(id)
                         .queryParam("key", apiToken)
                         .build())
                 .retrieve()
-                .bodyToMono(String.class)
+                .bodyToMono(PlantsResponse.Plant.class)
                 .block();
+
+        return preperePlant(Objects.requireNonNull(plant));
+    }
+
+    private List<PlantsResponseToFrontend> preparePlantsForFronted(List<PlantsResponse.Plant> plants) {
+        List<PlantsResponseToFrontend> plantsResponseToFrontends = new ArrayList<>();
+        for (PlantsResponse.Plant plant : plants) {
+            PlantsResponseToFrontend plantResponse = preperePlant(plant);
+            plantsResponseToFrontends.add(plantResponse);
+        }
+        return plantsResponseToFrontends;
+    }
+
+    private PlantsResponseToFrontend preperePlant(PlantsResponse.Plant plant) {
+        PlantsResponseToFrontend plantResponse = new PlantsResponseToFrontend();
+        plantResponse.setId(String.valueOf(plant.getId()));
+        plantResponse.setCommonName(plant.getCommonName());
+        plantResponse.setOriginalUrl(plant.getDefaultImage().getOriginalUrl());
+        return plantResponse;
     }
 }
