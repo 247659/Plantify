de238e76f55099bb2d2e2a84dbeb7efe0fcb3370
CountToFour <247806@edu.p.lodz.pl>
2025-05-28 17:53:23 +0200

    chatService

--- plantify/backend/src/main/java/project/plantify/AI/controllers/ChatController.java
+++ plantify/backend/src/main/java/project/plantify/AI/controllers/ChatController.java
@@ -1,89 +1,26 @@
 package project.plantify.AI.controllers;
 
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.chat.memory.MessageWindowChatMemory;
-import org.springframework.ai.chat.messages.AssistantMessage;
-import org.springframework.ai.chat.messages.Message;
-import org.springframework.ai.chat.messages.MessageType;
-import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Flux;
-
-import java.util.List;
+import project.plantify.AI.payloads.response.ChatResponse;
+import project.plantify.AI.services.ChatService;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/api/plantify/chat")
 public class ChatController {
 
-    private final ChatClient chatClient;
-    private final int MAX_SESSIONS = 20;
-    private final Map<String, ChatMemory> chatMemory;
-
-    public ChatController(ChatClient.Builder builder) {
-        this.chatClient = builder.build();
-        this.chatMemory = new ConcurrentHashMap<>();
-    }
+    @Autowired
+    private ChatService chatService;
 
     @PostMapping("/generate")
-    public String chat(@RequestBody Map<String, String> body,
-                       @RequestParam(value = "userId") String userId) {
-        ChatMemory singleChatMemory = getOrCreateChatMemory(userId);
+    public ResponseEntity<ChatResponse> chat(@RequestBody Map<String, String> body,
+                               @RequestParam(value = "userId") String userId) {
 
         String message = body.get("mes");
-        if (message == null || message.isEmpty()) {
-            throw new IllegalArgumentException("Message cannot be empty");
-        }
-
-        singleChatMemory.add(userId, new UserMessage(message));
-
-        System.out.println(singleChatMemory.get(userId).getFirst().getMessageType());
-        System.out.println(singleChatMemory.get(userId).getFirst().getText());
-        System.out.println("Received message: " + message);
-
-        String prompt = buildPrompt(singleChatMemory.get(userId));
-        System.out.println(prompt);
-        String response = chatClient.prompt().user(prompt)
-                .call()
-                .content();
-
-        singleChatMemory.add(userId, new AssistantMessage(response));
-
-        return response;
+        return ResponseEntity.ok(chatService.chat(message, userId));
     }
 
-    private String buildPrompt(List<Message> messages) {
-        StringBuilder prompt = new StringBuilder();
-
-        for (Message message : messages) {
-            System.out.println(message);
-            if (message.getMessageType() == MessageType.USER) {
-                prompt.append("User: ").append(message.getText()).append("\n");
-            } else if (message.getMessageType() == MessageType.ASSISTANT) {
-                prompt.append("Assistant: ").append(message.getText()).append("\n");
-            }
-        }
-        return prompt.toString();
-    }
-
-    private ChatMemory getOrCreateChatMemory(String sessionId) {
-        if (chatMemory.size() >= MAX_SESSIONS) {
-            chatMemory.clear();
-        }
-
-        return chatMemory.computeIfAbsent(sessionId, id -> {
-            System.out.println("Creating new chat memory for session: " + id);
-            return MessageWindowChatMemory.builder().build();
-        });
-    }
-
-
-    @GetMapping("/stream")
-    public Flux<String> stream(@RequestParam("mes") String message) {
-        System.out.println("Received message for streaming: " + message);
-        return chatClient.prompt().user(message).stream().content();
-    }
 }
---
+++ plantify/backend/src/main/java/project/plantify/AI/services/ChatService.java
@@ -0,0 +1,84 @@
+package project.plantify.AI.services;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.MessageType;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.stereotype.Service;
+import project.plantify.AI.payloads.response.ChatResponse;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class ChatService {
+
+    private final ChatClient chatClient;
+    private final Map<String, ChatMemory> chatMemory;
+
+    public ChatService(ChatClient.Builder builder, Map<String, ChatMemory> chatMemory) {
+        this.chatClient = builder.build();
+        this.chatMemory = chatMemory;
+    }
+
+    public ChatResponse chat(String message, String userId) {
+        ChatMemory singleChatMemory = getOrCreateChatMemory(userId);
+
+        if (message == null || message.isEmpty()) {
+            throw new IllegalArgumentException("Message cannot be empty");
+        }
+
+        if (singleChatMemory.get(userId).size() >= 10) {
+            singleChatMemory.get(userId).removeFirst();
+        }
+
+        singleChatMemory.add(userId, new UserMessage(message));
+
+        System.out.println(singleChatMemory.get(userId).getFirst().getMessageType());
+        System.out.println(singleChatMemory.get(userId).getFirst().getText());
+        System.out.println("Received message: " + message);
+
+        String prompt = buildPrompt(singleChatMemory.get(userId));
+        System.out.println(prompt);
+        String response = chatClient.prompt().user(prompt)
+                .call()
+                .content();
+
+        if (response == null || response.isEmpty()) {
+            throw new IllegalStateException("Response from chat client is empty");
+        }
+        singleChatMemory.add(userId, new AssistantMessage(response));
+
+        return new ChatResponse("assistant", response);
+    }
+
+    private String buildPrompt(List<Message> messages) {
+        StringBuilder prompt = new StringBuilder();
+
+        for (Message message : messages) {
+            System.out.println(message);
+            if (message.getMessageType() == MessageType.USER) {
+                prompt.append("User: ").append(message.getText()).append("\n");
+            } else if (message.getMessageType() == MessageType.ASSISTANT) {
+                prompt.append("Assistant: ").append(message.getText()).append("\n");
+            }
+        }
+        return prompt.toString();
+    }
+
+    private ChatMemory getOrCreateChatMemory(String sessionId) {
+        int MAX_SESSIONS = 20;
+        if (chatMemory.size() >= MAX_SESSIONS) {
+            chatMemory.clear();
+        }
+
+        return chatMemory.computeIfAbsent(sessionId, id -> {
+            System.out.println("Creating new chat memory for session: " + id);
+            return MessageWindowChatMemory.builder().build();
+        });
+    }
+
+}
